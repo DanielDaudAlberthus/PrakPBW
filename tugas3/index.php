57427734c217a8bd941413bbf5cfac67ba3cc54d
Daniel Daud Alberthus <144523084+DanielDaudAlberthus@users.noreply.github.com>
2024-10-12 20:01:28 +0700

    update tugas 3

---
+++ tugas3/index.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Landing Page - Buku Tamu</title>
+    <link rel="stylesheet" href="css/style.css">
+</head>
+<body>
+    <div class="container">
+        <h1 class="judul">Selamat Datang di Buku Tamu</h1>
+        <p>Selamat datang di situs buku tamu kami. Silakan mengisi buku tamu atau melihat daftar tamu yang telah mengisi.</p>
+        <ul>
+            <li>
+                <button> <a href="input.php">Isi Buku Tamu</a></button>
+               
+            </li>
+            <li>
+                <button><a href="table.php">Lihat Daftar Tamu</a></button>
+                
+            </li>
+        </ul>
+    </div>
+</body>
+</html>
