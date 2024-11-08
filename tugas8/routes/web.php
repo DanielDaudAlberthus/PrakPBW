a7dfda33b26959ac32f8ed756bbeeb9fed14624b
Daniel Daud Alberthus <danilalbert03@gmail.com>
2024-11-08 19:59:02 +0700

    update tugas 8

---
+++ tugas8/routes/web.php
@@ -0,0 +1,25 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+use App\Http\Controllers;
+
+// Route::get('/', function () {
+//     return view('home');
+// });
+
+// Route::get('/about', function() {
+//     return view('about');
+// });
+
+Route::get('/', Controllers\HomeController::class);
+Route::get('/about', [Controllers\AboutController::class, 'index']);
+Route::get('/contact', [Controllers\ContactController::class, 'index']);
+Route::get('/gallery', [Controllers\GalleryController::class, 'index']);
+
+Route::get('users', function () {
+    $users = [
+        ['id' => 1, 'name' => 'John Doe', 'email' => 'jane@parsinta.com'],
+        ['id' => 2, 'name' => 'Jane  Doe', 'email' => 'jane@parsinta.com'],
+    ];
+    return view('users.index', compact('users'));
+});
